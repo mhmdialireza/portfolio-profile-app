34cd651ef917ac304a9b3da5133a601c305287c8
Alireza Mohammadi <mhmdi.alireza8035@gmail.com>
2023-09-02 16:43:43 +0330

    Add Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './Components/Header';
+import Hero from './Components/Hero';
 import css from './Styles/App.module.scss';
 
 const App = () => {
   return (
     <div className={`bg-primary ${css.container}`}>
       <Header />
+      <Hero />
     </div>
   );
 };
